2623a64b1bdd06db2c7b678d9db6cadd12cd8613
hhdaghghdjnjncsnjcks3211cnsjbcbqjcbGARs <147187268+hhdaghghdjnjncsnjcks3211cnsjbcbqjcbGARs@users.noreply.github.com>
2023-10-07 22:56:44 +0200

    Update 14433762945.lua

--- 14433762945.lua
+++ 14433762945.lua
@@ -1,1 +1,30 @@
-print("Dick sucking man haha")
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local MarketplaceService = game:GetService("MarketplaceService")
+local productInfo = MarketplaceService:GetProductInfo(game.PlaceId)
+
+local Version = "0.1"
+
+local Window = Fluent:CreateWindow({
+    Title = "Sapphire Hub | " .. productInfo.Name.. " ".. Version,
+    SubTitle = "by dawid",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local Options = Fluent.Options
+
+
+do
+    
+end
